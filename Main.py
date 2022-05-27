2166192bf9990e65e9df0e752fee9988c062ff4b
AsakeGe <106314429+AsakeGe@users.noreply.github.com>
2022-05-27 16:58:51 +0800

    Add GUI

---
+++ Main.py
@@ -0,0 +1,34 @@
+import sys
+import RTMP_Server_Conf
+import UI.GUI as ui
+from PyQt5.QtWidgets import QApplication,QMainWindow,QMessageBox
+from PyQt5.QtCore import Qt
+from functools import partial
+
+def run():
+    Stream_Name = ui.Stream_Name.text()
+    RTMP_Server_Port=ui.RTMP_Server_Port.text()
+    Max_Link_Connection=ui.Max_Link_Connection.text()
+    Trunk_Block=ui.Trunk_Block.text()
+    Status_Port=ui.Status_Port.text()
+    result = RTMP_Server_Conf.MAIN(Stream_Name,RTMP_Server_Port,Max_Link_Connection,Trunk_Block,Status_Port)
+    ui.RTMP_Address.setText(str(result[0]))
+    ui.RTMP_Key.setText(str(result[1]))
+    ui.Live_Watch_Address.setText(str(result[2]))
+    ui.Server_Status_Address.setText(str(result[3]))
+    RTMP_Server_Conf.RUN_CMD(1)
+
+if __name__=='__main__':
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    ui = ui.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+
+    ui.IP_show.setText(str(RTMP_Server_Conf.Get_Host_IP()))
+
+    ui.Run.clicked.connect(run)
+
+
+    sys.exit(app.exec_())
